3845f2a9417d34cb1e66c78c3c7012ca5ac75c31
samshal <samueladeshina73@gmail.com>
2020-08-19 08:49:03 +0100

    Added dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 setup(
     name="keras_custom_ImageDataGenerator",
     version="1.0",
-    packages=find_packages(),
+    packages=['rasterio', 'numpy', 'scipy', 'PIL'],
 )
